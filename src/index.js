30b49d0a74585801a760739c5ec5ec479692e982
Prem4u <prem.max83@gmail.com>
2018-04-08 22:41:44 +0530

    added browser test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import {Person} from 'person.js';
+import { Person } from 'person.js';
 
-let person=new Person('Prem',30);
+const NAME = "Name";
+const AGE = "Age";
+let person1 = new Person('Prem', 30);
+let person2 = new Person('Raj', 25);
+let person3 = new Person('Arv', 2);
+let persons = [ person1, person2, person3 ];
+let createSimpleTableInBrowser = () => {
+	let center = document.createElement("center");
+	document.body.appendChild(center);
+	let h2 = document.createElement("h3");
+	let h2Text = document.createTextNode("Person's Information");
+	h2.appendChild(h2Text),
+	center.appendChild(h2);
+	let table = document.createElement("table");
+	let row = table.insertRow(0);
+	let cell1 = row.insertCell(0);
+	let cell2 = row.insertCell(1);
+	cell1.innerHTML = NAME;
+	cell2.innerHTML = AGE;
+	persons.forEach(function(elt, i, array) {
+		row = table.insertRow(1);
+		cell1 = row.insertCell(0);
+		cell2 = row.insertCell(1);
+		cell1.innerHTML = elt.getName();
+		cell2.innerHTML = elt.getAge();
+	})
+	center.appendChild(table);
 
-let test=()=>{
-	console.log("Test Successfull !! ");	
-	console.log("Name->"+person.getName()+" "+"Age ->"+person.getAge());
+	console.log("Test Successfull !! ");
+
 };
-class Employee extends Person{
-	 constructor(name,age){
-		 super(name,age);
-	 }
-}
-let emp1=new Employee();
-test();
 
-
+createSimpleTableInBrowser();
